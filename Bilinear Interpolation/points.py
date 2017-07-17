Fixed Partial Derivatives
Fixed the calculations in the partial derivatives. Used 2 functions to
test the program: f = sin(x + y), f = sin(x) * sin(y).

Bilinear Interpolation/points.py
@@ -4,9 +4,9 @@ import math
 x_array = np.linspace(-math.pi, math.pi, 100)
 y_array = np.linspace(-1, 1, 5)
 
-file = open('input0.txt','w')
+file = open('input1.txt','w')
 
 for y in y_array:
 	for x in x_array:
-		f = math.sin(x + y)
+		f = math.sin(x) * math.sin(y)
 		print >> file, str(f) + " " + str(x) + " " + str(y)